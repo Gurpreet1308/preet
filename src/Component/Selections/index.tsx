0576dcc5f18ca495bec6e47c4d633d397f3d5740
Gurpreet1308 <gurpreet2113singh@gmail.com>
2025-11-26 11:09:39 +0530

    wedfsd

---
+++ src/Component/Selections/index.tsx
@@ -0,0 +1,17 @@
+"use client";
+import Image from "next/image";
+
+export default function Selections{
+    return(
+        <div>
+            <div>
+             <Image
+                  src="/images/logo/sed-logo.png"
+                  alt="SED - Spray Engineering Devices Ltd Limited"
+                  width={200}
+                  height={100}
+                />
+            </div>
+        </div>
+    )
+}
